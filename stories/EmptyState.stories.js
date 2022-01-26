660f8f77e3b8dd6bb4568ddf08f9ad0c773e4b25
Martin Hradil <mhradil@redhat.com>
2022-01-26 12:27:31 +0000

    EmptyState storybook

---
+++ stories/EmptyState.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from '@patternfly/react-core';
+import { EmptyStateCustom } from '../src/EmptyStateCustom';
+import { EmptyStateFilter } from '../src/EmptyStateFilter';
+import { EmptyStateNoData } from '../src/EmptyStateNoData';
+import { EmptyStateUnauthorized } from '../src/EmptyStateUnauthorized';
+
+export default {
+  title: 'EmptyState',
+};
+
+export const Custom = (props) => <EmptyStateCustom {...props} />;
+export const Filter = (props) => <EmptyStateFilter {...props} />;
+export const NoData = (props) => <EmptyStateNoData {...props} />;
+export const Unauthorized = (props) => <EmptyStateUnauthorized {...props} />;
+
+Custom.args = {
+  button: <Button>Foobar</Button>,
+  description: 'Lorem ipsum dolor',
+  title: 'Quux',
+};
+
+Filter.args = {
+  clearAllFilters: () => console.log('cleared'),
+};
+
+NoData.args = {
+  button: 'Upload',
+  description: 'Collections will appear once uploaded',
+  title: 'No collections yet',
+};
+
+Unauthorized.args = {
+  loginLink: <Button>Foo</Button>,
+};
