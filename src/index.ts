63f9b0ce540255d28e2a2ad3fbe3801050a349e9
Zita Nemeckova <znemecko@redhat.com>
2022-07-15 13:14:15 +0200

    Add 500 error component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Error404 } from './Error404';
+export { Error500 } from './Error500';
 export { EmptyStateCustom } from './EmptyStateCustom';
 export { EmptyStateFilter } from './EmptyStateFilter';
 export { EmptyStateNoData } from './EmptyStateNoData';
